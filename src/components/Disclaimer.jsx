67cf577336b3fd56bbc9d358da762f3690834e2c
Jerome Checketts <jeromechecketts@gmail.com>
2023-04-18 21:16:59 -0500

    Updated Terms, Privay Policy, Disclaimer and Copyright

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -29,44 +29,26 @@ const Disclaimer = ({ darkTheme }) => {
             />
           </div>
           <div className="modal-body p-4">
-            <p>
-              Simply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book.
-            </p>
-            <ul className="lh-lg">
-              <li>
-                Lisque persius interesset his et, in quot quidam persequeris
-                vim, ad mea essent possim iriure.
-              </li>
-              <li>
-                Quidam lisque persius interesset his et, Lisque persius
-                interesset his et, in quot quidam persequeris vim, ad mea essent
-                possim iriure.
-              </li>
-              <li>
-                In quot quidam persequeris vim, ad mea essent possim iriure.
-                Quidam lisque persius interesset his et.
-              </li>
-              <li>
-                Quidam lisque persius interesset his et, Lisque persius
-                interesset his et.
-              </li>
-              <li>
-                Interesset his et, Lisque persius interesset his et, in quot
-                quidam persequeris vim, ad mea essent possim iriure.
-              </li>
-              <li>
-                Persius interesset his et, Lisque persius interesset his et, in
-                quot quidam persequeris vim, ad mea essent possim iriure.
-              </li>
-              <li>
-                Quot quidam persequeris vim Quidam lisque persius interesset his
-                et, Lisque persius interesset his et, in quot quidam persequeris
-                vim, ad mea essent possim iriure.
-              </li>
-            </ul>
+            <h3 className={"mb-3 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Copyright Statement:
+            </h3>
+              <p>
+                All content, including text, graphics, images, logos, and software, on this personal portfolio website is the property of the website owner or its content suppliers and is protected by copyright laws. You may not use, reproduce, or distribute any content from this website without the prior written permission of the website owner.
+              </p>
+            <h3 className={"mb-3 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Disclaimer:
+            </h3>
+              <p>
+              The information provided on this personal portfolio website is for general informational purposes only. The website owner makes no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability of the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+              <br /><br />
+              In no event will the website owner be liable for any loss or damage including, without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+              <br /><br />
+              Through this website you may be able to link to other websites which are not under the control of the website owner. The inclusion of any links does not necessarily imply a recommendation or endorsement of the views expressed within them.
+              <br /><br />
+              Every effort is made to keep the website up and running smoothly. However, the website owner takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+              <br /><br />
+              By using this personal portfolio website, you agree to indemnify and hold harmless the website owner from any and all claims, damages, expenses, and costs, including reasonable attorneys' fees, arising from your use of the website or your violation of these terms and conditions.
+              </p>
           </div>
         </div>
       </div>
--- src/components/TermsAndConditions.jsx
+++ src/components/TermsAndConditions.jsx
@@ -29,89 +29,117 @@ const TermsAndConditions = ({ darkTheme }) => {
             />
           </div>
           <div className="modal-body p-4">
-            <p>
+            {/* <p>
               Simply dummy text of the printing and typesetting industry. Lorem
               Ipsum has been the industry's standard dummy text ever since the
               1500s, when an unknown printer took a galley of type and scrambled
               it to make a type specimen book. It has survived not only five
               centuries, but also the leap into electronic typesetting,
               remaining essentially unchanged.
-            </p>
-            <h3 className={"mb-3 mt-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+            </p> */}
+            <h3 className={"mb-3 mt-2 " + (darkTheme ? "text-white" : "")}>
               Terms of Use
             </h3>
-            <p>
+            {/* <p>
               It has survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. Simply
               dummy text of the printing and typesetting industry.
-            </p>
+            </p> */}
             <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
-              Part I – Information Simone collects and controls
+              Introduction
             </h5>
             <p>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
+              Welcome to my portfolio website. By accessing and using my website, you agree to be bound by these terms of use, my privacy policy, and all applicable laws and regulations.
             </p>
             <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
-              Part II – Information that Simone processes on your behalf
+              Intellectual Property Rights
             </h5>
             <p>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
+              All content on my website, including but not limited to text, graphics, images, logos, and software, is the property of my website or its content suppliers and is protected by intellectual property laws. You may not use, reproduce, or distribute any content from my website without my prior written permission.
             </p>
             <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
-              Part III – General
+              User Conduct
             </h5>
             <p>
-              It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book.
+              You agree to use my website only for lawful purposes and in a manner that does not infringe on the rights of others or restrict or inhibit their use and enjoyment of the website. You may not use my website to transmit or distribute any content that is unlawful, defamatory, obscene, or that infringes on any intellectual property rights.
+            </p>
+            <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Limitation of Liability
+            </h5>
+            <p>
+              My website is provided on an "as is" and "as available" basis. We make no warranties, express or implied, regarding the operation of my website or the content, information, or services provided on my website. We will not be liable for any damages of any kind arising from the use of my website, including but not limited to direct, indirect, incidental, punitive, and consequential damages.
+            </p>
+            <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Indemnification
+            </h5>
+            <p>
+              You agree to indemnify and hold harmless my website and its affiliates, officers, directors, employees, and agents from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of my website or your violation of these terms of use.
+            </p>
+            <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Termination
+            </h5>
+            <p>
+              I reserve the right to terminate your access to our website at any time without notice if I determine, in my sole discretion, that you have violated these terms of use or any applicable laws or regulations.
+            </p>
+            <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+              Governing Law
+            </h5>
+            <p>
+              These terms of use are governed by and construed in accordance with the laws of Texas. Any dispute arising out of or related to these terms of use will be submitted to the exclusive jurisdiction of the courts of Texas.
             </p>
             <h3 className={"mb-3 mt-4 " + (darkTheme ? "text-white" : "")}>
               Privacy Policy
             </h3>
-            <p>
+            {/* <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
-            </p>
-            <ol className="lh-lg">
+            </p> */}
+            <ul className="lh-lg list-unstyled">
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Introduction
+              </h5>
               <li>
-                Lisque persius interesset his et, in quot quidam persequeris
-                vim, ad mea essent possim iriure.
+                We respect your privacy and are committed to protecting your personal information. This privacy policy explains how we collect, use, and disclose your personal information when you use our portfolio website.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Information Collection
+              </h5>
               <li>
-                Quidam lisque persius interesset his et, Lisque persius
-                interesset his et, in quot quidam persequeris vim, ad mea essent
-                possim iriure.
+                We collect personal information that you voluntarily provide to us, such as your name, email address, and other contact information. We may also collect information about your use of our website through cookies and other tracking technologies.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Use of Information
+              </h5>
               <li>
-                In quot quidam persequeris vim, ad mea essent possim iriure.
-                Quidam lisque persius interesset his et.
+                We use your personal information to provide you with information about our services and to communicate with you. We may also use your information to improve our website and to analyze how users interact with our website.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Disclosure of Information
+              </h5>
               <li>
-                Quidam lisque persius interesset his et, Lisque persius
-                interesset his et.
+                We may disclose your personal information to third-party service providers who perform services on our behalf, such as web hosting and analytics. We may also disclose your information if required by law or if we believe that such disclosure is necessary to protect our rights or the rights of others.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Security
+              </h5>
               <li>
-                Interesset his et, Lisque persius interesset his et, in quot
-                quidam persequeris vim, ad mea essent possim iriure.
+                We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the internet or electronic storage is 100% secure.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Children's Privacy
+              </h5>
               <li>
-                Persius interesset his et, Lisque persius interesset his et, in
-                quot quidam persequeris vim, ad mea essent possim iriure.
+                Our website is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under the age of 13.
               </li>
+              <h5 className={"text-4 mt-4 " + (darkTheme ? "text-white" : "")}>
+                Changes to this Policy
+              </h5>
               <li>
-                Quot quidam persequeris vim Quidam lisque persius interesset his
-                et, Lisque persius interesset his et, in quot quidam persequeris
-                vim, ad mea essent possim iriure.
+                We reserve the right to update this privacy policy from time to time. We will post the updated policy on our website and indicate the date of the latest revision. We encourage you to review the policy periodically to stay informed about our information practices. Your continued use of our website after we make changes to the policy will signify your acceptance of the updated policy.
               </li>
-            </ol>
+            </ul>
           </div>
         </div>
       </div>
